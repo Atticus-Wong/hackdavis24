96c148cfad22f3567a31f7bfd8e3b02c9bd9e84d
Atticus Wong <atticuswong@gmail.com>
2024-04-27 18:50:58 -0700

    modified run function to accept string parameter

--- backend/prompt.js
+++ backend/prompt.js
@@ -4,15 +4,15 @@ require('dotenv').config()
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-async function run() {
+async function run(input_from_user) {
   // For text-only input, use the gemini-pro model
 
   console.time("executionTime");
 
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  const private_policy = "";
-  const prompt = private_policy + "Summarize this text. What are the important things I should know about? Is there anything unethical about it?";
+  const private_policy = input_from_user;
+  const prompt = private_policy + " Summarize this text. What are the important things I should know about? Is there anything unethical about it?";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
