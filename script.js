832241e83108f38aa6f3cee396be563a9d454fae
Atticus Wong <atticuswong@gmail.com>
2024-04-27 18:43:50 -0700

    adding script.js

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ document.getElementById('GenerateButton').addEventListener("click", function() {
 
   const priv_policy = document.getElementById("priv-pol").value;
   const answer = run(priv_policy);
+
+  document.getElementById("priv-pol").textContent = answer;
 });
 
